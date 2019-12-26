9bfa4f7a0e7200011fb9ebe3ca939222056e1f1a
Aaron McKoy <mckoy_aaron@yahoo.com>
2019-12-26 17:03:48 -0500

    Added new db name in server, axios routes in API.js, todos model , controller, routes.

---
+++ models/todo.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const todoSchema = new Schema ({
+    title: { type: String, required: true },
+    creator: { type: String, required: true },
+    startTime: { type: Date, default: Date.now },
+    endTime: { type: Date, default: Date.now().addHours(1) },
+    description: { type: String }
+    
+
+})
