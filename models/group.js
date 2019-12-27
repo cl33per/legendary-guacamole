5dd4df0bc412271bf93d8fd5b9a39572b851cd93
Aaron McKoy <mckoy_aaron@yahoo.com>
2019-12-26 19:15:37 -0500

    Deleted a comment that was no longer needed.

--- models/group.js
+++ models/group.js
@@ -6,9 +6,7 @@ var Schema = mongoose.Schema;
 // Using the Schema constructor, create a new GroupSchema object
 // This is similar to a Sequelize model
 var GroupSchema = new Schema({
-  // `name` must be of type String
-  // `name` must be unique, the default mongoose error message is thrown if a duplicate value is given
-  name: {
+    name: {
     type: String,
     unique: true
   },
