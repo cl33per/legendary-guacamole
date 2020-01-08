4d7eef98b75e95228e0f266cd75366e36cc1aadb
cl33per <leeper08@gmail.com>
2020-01-08 11:08:18 -0500

    added routes for users.

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,10 @@ const usersController = require("../../controllers/usersController");
 
 // TODO: Need to configure and understand routes to DB and how each realationsip. 
 // Matches with "/api/users/register"
+router.route("/")
+    .get(usersController.findAll)
+    .post(usersController.create);
+
 router.route("/register")
     .post(usersController.register);
 
