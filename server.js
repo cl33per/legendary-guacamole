aad01f0b80f95a3de6c89eba5911312c8343d173
Aaron McKoy <mckoy_aaron@yahoo.com>
2020-01-16 21:31:11 -0500

    Removed multer from server, moved to upload.js .

--- server.js
+++ server.js
@@ -5,11 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const passport = require("passport");
 const bodyParser = require("body-parser");
-require('dotenv').config();
+require("dotenv").config();
 const cors = require("cors");
-const multer = require("multer");
-
-
+// const multer = require("multer");
 
 // Bodyparser middleware
 app.use(
@@ -22,43 +20,49 @@ app.use(bodyParser.json());
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+} else {
+  app.use(express.static("public"));
 }
+
 // Add routes, both API and view
 app.use(routes);
 
 //Added to resolve a depercation issue
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useUnifiedTopology', true );
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useUnifiedTopology", true);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/familyties")
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/familyties")
   .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));;
+  .catch(err => console.log(err));
 
-  // Multer Upload
-var storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-  cb(null, 'public/images/uploads')
-},
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname)
-}
-});
-const upload = multer({ storage })
+// Multer Upload
+// var storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "admin/public/images/uploads");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + "-" + file.originalname);
+//   }
+// });
+// const upload = multer({ storage });
 
 app.use(cors());
 
-app.post('/upload', upload.single('image'), (req, res) => {
-if (req.file)
-  res.json({
-    imageUrl: `images/uploads/${req.file.filename}`
-});
-else
-  res.status("409").json("No Files to Upload.");
-});
+// app.post('api/upload', upload.single('image'), (req, res) => {
+//   console.log("Server upload:")
+// if (req.file)
+//   res.json({
+//     imageUrl: `images/uploads/${req.file.filename}`
+// });
+// else
+//   res.status("409").json("No Files to Upload.");
+// });
 
 // Passport middleware
 app.use(passport.initialize());
