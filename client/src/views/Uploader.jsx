8d63182dbe88c5bdde82f1863df802d63c6f9a35
Aaron McKoy <mckoy_aaron@yahoo.com>
2020-01-16 21:38:15 -0500

    Revised Uploader component and Uploader view.

--- client/src/views/Uploader.jsx
+++ client/src/views/Uploader.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import API from "../../utils/API"
 import axios from 'axios';
-const BASE_URL = 'http://localhost:3001/';
+// const BASE_URL = 'http://localhost:3000/';
  
 class Uploader extends Component {
     constructor(props) {
@@ -14,6 +14,7 @@ class Uploader extends Component {
     }
 
     selectImages = (event) => {
+        console.log(event.target.files[0]);
         let images = []
         for (var i = 0; i < event.target.files.length; i++) {
             images[i] = event.target.files.item(i);
@@ -29,12 +30,16 @@ class Uploader extends Component {
         data.append("image", image, image.name);
     
         // Make an AJAX upload request using Axios
-        return axios.post(BASE_URL + 'upload', data)
-        .then(response => {
-            this.setState({
-            imageUrls: [ response.data.imageUrl, ...this.state.imageUrls ]
-            });
-        })
+        return axios.post('/api/upload', data, {
+            onUploadProgress: ProgressEvent => {
+                console.log('Upload Progress:' + Math.round(ProgressEvent.loaded / ProgressEvent.total * 100) + '%')
+                }
+            })
+            .then(response => {
+                this.setState({
+                imageUrls: [ response.data.imageUrl, ...this.state.imageUrls ]
+                });
+            })
       
     });
     
@@ -53,9 +58,11 @@ class Uploader extends Component {
     //       .catch(err => console.log(err));
     //   };
     render() {
+        console.log(this.state.imageUrls);
         return (
         <div>
             <div className="col-sm-12">
+            <img src="/images/uploads/1579225955755-20191031_191602.jpg" style={{width: '200px', height: '200px'}} className="img-rounded img-responsive" />
                 <h1>Image Uploader</h1><hr/>
                 <div className="col-sm-4">
                     <input className="form-control " type="file" onChange={this.selectImages} multiple/>
@@ -69,13 +76,16 @@ class Uploader extends Component {
 
 
             <div className="row col-lg-12">
-                {
-                this.state.imageUrls.map((url, i) => (
-                <div className="col-lg-2" key={i}>
-                    <img src={BASE_URL + url} className="img-rounded img-responsive" alt="not available"/><br/>
-                </div>
-                ))
+                {this.state.imageUrls.map((url, i) => {
+                    console.log(url);
+                    return (
+                        <div className="col-lg-2" key={i}>
+                            <img src={url} className="img-rounded img-responsive" alt="wsup?" />
+                            <br/>
+                        </div>
+                    );
                 }
+                )}
             </div>
         </div>
         );
