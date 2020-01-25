7929b72b497172e7c4cc5eb06e93ab9eab07f6f3
cl33per <leeper08@gmail.com>
2020-01-25 12:35:46 -0500

    links navbar.

--- client/src/components/Navbars/AdminNavbarLinks.jsx
+++ client/src/components/Navbars/AdminNavbarLinks.jsx
@@ -36,10 +36,10 @@ class AdminNavbarLinks extends Component {
         </Nav>
         <Nav pullRight>
           <NavDropdown eventKey={2} title="Helpful Links" id="basic-nav-dropdown-right">
-            <Link to="register"><MenuItem eventKey={2.1}>Register</MenuItem></Link>
-            <Link to="login"><MenuItem eventKey={2.1}>Login</MenuItem></Link>
-            <Link to="landing"><MenuItem eventKey={2.1}>RegisCalendarter</MenuItem></Link>
-            <Link to="calendar"><MenuItem eventKey={2.1}>Calendar</MenuItem></Link>
+            <MenuItem eventKey={2.1}><Link to="register">Register</Link></MenuItem>
+            <MenuItem eventKey={2.2}><Link to="Login">Login</Link></MenuItem>
+            <MenuItem eventKey={2.3}><Link to="landing">Landing Page</Link> </MenuItem>
+            <MenuItem eventKey={2.4}><Link to="calendar">Calendar</Link></MenuItem>
           </NavDropdown>
           <Link to="user"><i className="fa fa-user"/>Profile</Link>
           <NavItem id="logout" eventKey={3} onClick={this.onLogoutClick}>Log out</NavItem>
